b9d65818810c2c9a412a095431021695bf626c43
bobdm4 <dem4enkoborys@gmail.com>
2019-11-25 13:13:58 +0200

    merge_hw1

--- src/routes/products/product.js
+++ src/routes/products/product.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const productsRoute = (request, response) => {
-  const productFilePath = path.join(__dirname, '../../db/products/all-products.json');
-  const products = fs.readFileSync(productFilePath);
+    const productFilePath = path.join(__dirname, '../../db/products/all-products.json');
+    const products = fs.readFileSync(productFilePath);
 
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.write(products);
-  response.end();
-  return;
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(products);
+    response.end();
+    return;
 };
 
 module.exports = productsRoute;
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,9 +3,9 @@ const productsRoute = require('./products/product');
 const signUpRoute = require('./users/sign-up-route');
 
 const router = {
-  '/products': productsRoute,
-  '/signup': signUpRoute,
-  default: mainRoute
+    '/products': productsRoute,
+    '/signup': signUpRoute,
+    default: mainRoute
 };
 
 module.exports = router;
